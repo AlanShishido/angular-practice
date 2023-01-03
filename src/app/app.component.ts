e225d09f649d357bc726644b01c7bb532ee26a1b
AlanShishido <shishido.hideyuki@gmail.com>
2023-01-03 10:16:16 -0400

    angular cycle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `
+    <app-title title="Olá Alguma coisa"></app-title>
+    <router-outlet></router-outlet>
+    `,
+  // templateUrl: './app.component.html',
+  // styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements	OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
+  constructor(){}
+
+  ngOnInit(): void {}
+  ngOnChanges(): void {}
+  ngDoCheck(): void {}
+  ngAfterContentInit(): void {}
+  ngAfterContentChecked(): void {}
+  ngAfterViewInit(): void {}
+  ngAfterViewChecked(): void {}
+  ngOnDestroy(): void {}
 }
